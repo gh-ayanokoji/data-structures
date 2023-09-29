9e154aefc8f9557ee9b6b04def4e1b5befa2270b
Ayan <106065442+gh-ayanokoji@users.noreply.github.com>
2023-09-29 10:32:42 +0530

    Create practical_01.py

---
+++ practical_01.py
@@ -0,0 +1,49 @@
+cricket = set()
+badminton = set()
+football = set()
+
+cric_st = int(input('enter the no. of students playing cricket: '))
+for i in range(cric_st):
+    roll = int(input("Enter the roll no. of student: "))
+    if roll not in cricket:
+        cricket.add(roll)
+    else:
+        pass
+
+bad_st = int(input('enter the no. of students playing badminton: '))
+for i in range(bad_st):
+    roll = int(input("Enter the roll no. of student: "))
+    if roll not in badminton:
+        badminton.add(roll)
+    else:
+        pass
+
+foot_st = int(input('enter the no. of students playing football: '))
+for i in range(foot_st):
+    roll = int(input("Enter the roll no. of student: "))
+    if roll not in football:
+        football.add(roll)
+    else:
+        pass
+
+def cricNBad(cricket, badminton):
+    cnb = cricket.intersection(badminton)
+    return cnb
+
+def notBothCnb(cricket, badminton):
+    ncnb = cricket.symmetric_difference(badminton)
+    return ncnb
+
+def neitherCNB(football, cricket, badminton):
+    fnc = (cricket.union(badminton)).symmetric_difference(football)
+    return fnc
+
+def fNCnotBad(football, cricket, badminton):
+    fnc = (football.intersection(cricket)).symmetric_difference(badminton)
+    return fnc
+
+print(f"Roll no. of students that play both cricket and badminton are: {cricNBad(cricket, badminton)}")
+print(f"Roll no. of students that play either cricket and badminton but not both are: {notBothCnb(cricket, badminton)}")
+print(f"Roll no. of students that play neither cricket nor badminton are: {neitherCNB(football, cricket, badminton)}")
+print(f"Roll no. of students that play both cricket and football but no badminton are: {fNCnotBad(football, cricket, badminton)}")
+
