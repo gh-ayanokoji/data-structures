61a28e013b2ebd62013fd1a8494567b30d3031f5
Ayan <106065442+gh-ayanokoji@users.noreply.github.com>
2023-10-03 11:52:25 +0530

    practical 03

--- practical_03.py
+++ practical_03.py
@@ -1,34 +1,41 @@
 total = int(input("Enter the no. of books: "))
-books = {}
+books = []
 for i in range(total):
-    book = input("Enter the name of book: ")
     cost = int(input("Enter the cost of book: "))
-    books[book] = cost
+    books.append(cost)
 
 print(books)
 
 def delDup(books):
-    noDup = {}
+    noDup = []
     for item in books:
         if item not in noDup:
-            noDup[item] = books[item]
+            noDup.append(item)
     return noDup
 
 def ascending(books):
-    asc = {}
-    for item in books:
-        for i in books.values():
-            for j in books.values():
-                if i < j:
-                    if i not in books.values():
-                        asc[item] = books[item]
-
-    return asc
+    books.sort()
+    return books
 
     
 def costly(books):
-    pass
+    count = 0
+    for item in books:
+        if item > 500:
+            count+=1
+
+    return count
+
+def lessCost(books):
+    newList = []
+    for item in books:
+        if item < 500:
+            newList.append(item)
+
+    return newList
 
 print(f"Books without duplicate entries is {delDup(books)}")
 print(f"Books in ascending order is {ascending(books)}")
+print(f"{costly(books)} no. of books cost more than 500")
+print(f"Books costing less than 500 are {lessCost(books)}")
 
