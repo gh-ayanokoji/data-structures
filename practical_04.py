b29f12fc613728578ce9a5cf63480604d6486d76
Ayan <106065442+gh-ayanokoji@users.noreply.github.com>
2023-11-02 12:05:23 +0530

    del

--- practical_04.py
+++
@@ -1,8 +0,0 @@
-bankAc = 0
-
-def deposit(bankAc, depo):
-    bankAc = bankAc + depo
-
-def withdraw(bankAc, wd):
-    if bankAc - wd < 0:
-        print(f"Your account does not have enough balance to withdraw Rs.{wd}")
